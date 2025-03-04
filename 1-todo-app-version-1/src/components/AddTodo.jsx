d7b454ed411f6cbacdfafd29ef6c6b73d2f7650c
enginr-abhi <abhishekthakur17021996@gmail.com>
2025-03-04 20:35:08 +0530

    Add new Folder

---
+++ 1-todo-app-version-1/src/components/AddTodo.jsx
@@ -0,0 +1,10 @@
+function AddTodo(){
+  return <div class="container ">
+  <div class="row kg-row">
+    <div class="col-6"><input type="text" placeholder = "Enter Todo Here"/></div>
+    <div class="col-4"><input type="date" name="" id="" /></div>
+    <div class="col-2"><button type="button" class="btn btn-success kg-button addbtn ">Add</button></div>
+  </div>
+</div>
+}
+export default AddTodo;
